af67e6cbe6172f56150e98b9f70b9328b91d87ac
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-30 19:09:58 +0530

    [Muskan] Added :- Created a StateCensusAnalyser Class to load the State Census CSV Data

---
+++ StateCensusAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCensusAnalyserProblem
+{
+    public class StateCensusAnalyser
+    {
+        private string filePath;
+
+        public StateCensusAnalyser(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<string[]> LoadData()
+        {
+            CSVStateCensus csvStateCensus = new CSVStateCensus(filePath);
+            var data = csvStateCensus.GetData();
+
+            List<string[]> dataList = new List<string[]>();
+            foreach (var item in data)
+            {
+                dataList.Add(item);
+            }
+
+            return dataList;
+        }
+    }
+}
