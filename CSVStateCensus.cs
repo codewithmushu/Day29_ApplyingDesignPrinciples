11652b618e3c3cd32e79f564c0b175f718f305cb
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-30 19:06:08 +0530

    [Muskan] Added : Created CSVStateCensus Class to load the CSV Data

---
+++ CSVStateCensus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCensusAnalyserProblem
+{
+    public class CSVStateCensus
+    {
+        private string filePath;
+        private List<string[]> data;
+
+        public CSVStateCensus(string filePath)
+        {
+            this.filePath = filePath;
+            data = new List<string[]>();
+        }
+
+        public IEnumerable<string[]> GetData()
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    yield return values;
+                }
+            }
+        }
+    }
+}
