5dada451b565f58980af3192a59cd5abca865cfb
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-30 19:13:19 +0530

    [Muskan] Added : Used Itrator to load the data.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCensusAnalyserProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StateCensusAnalyser analyser = new StateCensusAnalyser(@"C:\Users\91997\source\repos\StateCensusAnalyserProblem\Analyser.csv");
+            List<string[]> data = analyser.LoadData();
+            foreach (var item in data)
+            {
+                Console.WriteLine(string.Join(",", item));
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
