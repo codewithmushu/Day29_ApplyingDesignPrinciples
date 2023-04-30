b037d1f10b75631e0f7b408c65d2f54aa7bca21e
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-30 23:22:12 +0530

    [Muskan] Added : CSV format Exception.

--- CSVStateCensus.cs
+++ CSVStateCensus.cs
@@ -20,15 +20,40 @@ namespace StateCensusAnalyserProblem
 
         public IEnumerable<string[]> GetData()
         {
-            using (var reader = new StreamReader(filePath))
-            {
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
+            //using (var reader = new StreamReader(filePath))
+            //{
+            //    while (!reader.EndOfStream)
+            //    {
+            //        var line = reader.ReadLine();
+            //        var values = line.Split(',');
 
-                    yield return values;
+            //        yield return values;
+            //    }
+            //}
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"Could not find file at {filePath}");
+            }
+
+            string[] fileLines = File.ReadAllLines(filePath);
+            if (fileLines.Length < 2)
+            {
+                throw new CSVFormatException("File does not contain enough records");
+            }
+
+           
+
+            foreach (string line in fileLines)
+            {
+                var values = line.Split(',');
+
+                if (values.Length != 3)
+                {
+                    throw new CSVFormatException($"Invalid number of fields in line: {line}");
                 }
+                data.Add(values);
+                yield return values;    
             }
         }
 
@@ -44,6 +69,13 @@ namespace StateCensusAnalyserProblem
         public int GetNumberOfRecords()
         {
             return data.Count;
+        }
+    }
+
+    public class CSVFormatException : Exception
+    {
+        public CSVFormatException(string message) : base(message)
+        {
         }
     }
 }
