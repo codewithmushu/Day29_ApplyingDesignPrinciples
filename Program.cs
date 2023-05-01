8b5316e9c5f83d02e36a16ca5108e19a87347c01
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-05-01 12:52:22 +0530

    [Muskan] Added : Try and Catch Exception.

--- Program.cs
+++ Program.cs
@@ -31,23 +31,41 @@ namespace StateCensusAnalyserProblem
             //}
             //Console.ReadKey();
 
+
+
+            //try
+            //{
+            //    StateCensusAnalyser analyser = new StateCensusAnalyser(@"C:\Users\91997\source\repos\StateCensusAnalyserProblem\Analyser.csv");
+            //    int expectedRecords = 4;
+            //    int actualRecords = analyser.GetNumberOfRecodrs();
+            //    if (actualRecords == expectedRecords)
+            //    {
+            //        Console.WriteLine("Test case passed. Number of records match.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Test case failed. Expected {expectedRecords} records, but got {actualRecords}.");
+            //    }
+            //}
+            //catch (CSVFormatException e)
+            //{
+            //    Console.WriteLine($"Test case failed. {e.Message}");
+            //}
+
+
             try
             {
-                StateCensusAnalyser analyser = new StateCensusAnalyser(@"C:\Users\91997\source\repos\StateCensusAnalyserProblem\Analyser.csv");
-                int expectedRecords = 4;
-                int actualRecords = analyser.GetNumberOfRecodrs();
-                if (actualRecords == expectedRecords)
-                {
-                    Console.WriteLine("Test case passed. Number of records match.");
-                }
-                else
-                {
-                    Console.WriteLine($"Test case failed. Expected {expectedRecords} records, but got {actualRecords}.");
-                }
+                string filePath = @"C:\Users\91997\source\repos\StateCensusAnalyserProblem\Analyser.csv";
+                StateCensusAnalyser analyser = new StateCensusAnalyser(filePath);
+                analyser.LoadData();
             }
-            catch (CSVFormatException e)
+            catch (CensusAnalyserException ex)
             {
-                Console.WriteLine($"Test case failed. {e.Message}");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             Console.ReadKey();
 
