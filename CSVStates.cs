57995a49013e83c0f48912a21067cbbd716ba1f3
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-05-01 15:39:53 +0530

    [Muskan] Added : Ability for the analyser to load the Indian States Code Information from a csv file - Create CSVStates Class to load the CSV Data - Use Iterator to load the data - Check with StateCensusAnalyser to ensure number of record matches

---
+++ CSVStates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCensusAnalyserProblem
+{
+    class CSVStates : IEnumerable
+    {
+        private string filename;
+
+        public CSVStates(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            using (var reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line.Split(',');
+                }
+            }
+        }
+
+    }
+}
