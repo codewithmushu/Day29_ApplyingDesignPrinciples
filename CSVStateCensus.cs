82a49d08cab50b1af634d132fbc096241ed90b4d
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-05-01 14:05:56 +0530

    [Muskan] Added : Delimiter and throw exception

--- CSVStateCensus.cs
+++ CSVStateCensus.cs
@@ -9,48 +9,95 @@ namespace StateCensusAnalyserProblem
 {
     public class CSVStateCensus
     {
-        public IEnumerable<string[]> GetData(string filePath)
+        //public IEnumerable<string[]> GetData(string filePath)
+        //{
+        //    if (!File.Exists(filePath))
+        //    {
+        //        throw new FileNotFoundException("File not found!", filePath);
+        //    }
+
+        //    var lines = File.ReadAllLines(filePath);
+
+        //    if (lines.Length <= 1)
+        //    {
+        //        throw new InvalidDataException("File is empty or does not contain any data.");
+        //    }
+
+        //    var header = lines[0].Split(',');
+
+        //    // Check if the headers are valid
+        //    if (!header.SequenceEqual(new string[] { "State", "Population", "AreaInSqKm", "DensityPerSqKm" }))
+        //    {
+        //        throw new InvalidDataException("Invalid header! Header should contain State, Population, AreaInSqKm, and DensityPerSqKm in that order.");
+        //    }
+
+        //    for (int i = 1; i < lines.Length; i++)
+        //    {
+        //        var values = lines[i].Split(',');
+
+        //        // Check if the number of columns match with the header
+        //        if (values.Length != header.Length)
+        //        {
+        //            throw new InvalidDataException($"Line {i + 1} has invalid number of columns.");
+        //        }
+
+        //        // Check if the values are of correct type
+        //        if (!int.TryParse(values[1], out _) || !double.TryParse(values[2], out _) || !double.TryParse(values[3], out _))
+        //        {
+        //            throw new InvalidDataException($"Line {i + 1} has invalid data type.");
+        //        }
+
+        //        yield return values;
+        //    }
+
+        //}
+
+        public IEnumerable<string[]> GetData(string filePath, char delimiter)
         {
             if (!File.Exists(filePath))
             {
-                throw new FileNotFoundException("File not found!", filePath);
+                throw new CensusAnalyserException("File not found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
             }
 
-            var lines = File.ReadAllLines(filePath);
+            string[] lines = File.ReadAllLines(filePath);
 
-            if (lines.Length <= 1)
+            if (lines.Length == 0)
             {
-                throw new InvalidDataException("File is empty or does not contain any data.");
+                throw new CensusAnalyserException("File is empty", CensusAnalyserException.ExceptionType.EMPTY_FILE);
             }
 
-            var header = lines[0].Split(',');
+            var data = new List<string[]>();
 
-            // Check if the headers are valid
-            if (!header.SequenceEqual(new string[] { "State", "Population", "AreaInSqKm", "DensityPerSqKm" }))
+            foreach (var line in lines)
             {
-                throw new InvalidDataException("Invalid header! Header should contain State, Population, AreaInSqKm, and DensityPerSqKm in that order.");
-            }
-
-            for (int i = 1; i < lines.Length; i++)
-            {
-                var values = lines[i].Split(',');
-
-                // Check if the number of columns match with the header
-                if (values.Length != header.Length)
+                if (string.IsNullOrWhiteSpace(line))
                 {
-                    throw new InvalidDataException($"Line {i + 1} has invalid number of columns.");
+                    throw new CensusAnalyserException("Invalid line in file", CensusAnalyserException.ExceptionType.INVALID_LINE);
                 }
+                var values = line.Split(delimiter);
 
-                // Check if the values are of correct type
-                if (!int.TryParse(values[1], out _) || !double.TryParse(values[2], out _) || !double.TryParse(values[3], out _))
-                {
-                    throw new InvalidDataException($"Line {i + 1} has invalid data type.");
-                }
-
-                yield return values;
+                data.Add(values);
             }
+
+            return data;
 
         }
+    }
+     public class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND,
+            EMPTY_FILE,
+            INVALID_LINE
+           
+        }
 
+        public ExceptionType type;
+
+        public CensusAnalyserException(string message, ExceptionType type) : base(message)
+        {
+            this.type = type;
+        }
     }
 }
